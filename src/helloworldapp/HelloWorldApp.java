c399be6ec4ea3c2560fc60c7ad50985e873bbd16
John Gellert <gellertjm@gmail.com>
2019-10-19 21:44:49 -0500

    first commit

---
+++ src/helloworldapp/HelloWorldApp.java
@@ -0,0 +1,18 @@
+
+package helloworldapp;
+
+/**
+ *
+ * @author john.gellert
+ */
+public class HelloWorldApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("This message is brought to you by John Gellert");
+    }
+    
+}
